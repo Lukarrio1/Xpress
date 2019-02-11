ffb2f68bcfb31d3a535f1ff7492f71d426d81207
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-02-11 01:49:32 -0500

    change

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading text-center">Admin Dashboard</div>
+                <div class="panel-heading h3 text-center">Admin Dashboard</div>
                 <div class="panel-body">
                   Welcome admin am glad to see you
                 </div>
