b9cb952251e4e0b9d2e773bb9354391f69cd16d6
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-02-10 21:05:36 -0500

    admin section complete

---
+++ app/Http/Controllers/VerifyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\user;
+
+class VerifyController extends Controller
+{
+        // verify the user 
+        public function Verify($token){
+            User::where('token',$token)->firstOrFail()
+                ->update(['token'=>null]);
+                return redirect()->route('home')->with('success','Account Verified');
+           }
+}
