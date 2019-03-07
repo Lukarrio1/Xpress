463d423629cefdd98d166dc7f5091ea0276d7241
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-03-06 21:43:54 -0500

    login page fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,66 +3,82 @@
 @section('content')
 <div class="valign-wrapper row login-box">
   <div class="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4">
-    <form>
+    <form method="POST" action="{{ route('login') }}">
+      {{ csrf_field() }}
       <div class="card-content">
-        <h3 class="card-title  red-text">Xpress 
+        <h3 class="card-title center red-text">Xpress
           Logistics Login</h3>
         <div class="row">
           <div class="input-field col s12">
-            <label for="email">Email address</label>
-            <input type="email" class="validate" name="email" id="email" />
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+              <label for="email">Email address</label>
+              <input type="email" class="validate" name="email" id="email" value="{{ old('email') }}" required
+                autofocus>
+              @if ($errors->has('email'))
+              <span class="help-block">
+                <strong>{{ $errors->first('email') }}</strong>
+              </span>
+              @endif
+            </div>
+            <div class="input-field col s12">
+              <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password">Password </label>
+                <input type="password" class="validate" name="password" id="password" placeholder="Password" required />
+                @if ($errors->has('password'))
+                <span class="help-block">
+                  <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+
+              </div>
+            </div>
           </div>
-          <div class="input-field col s12">
-            <label for="password">Password </label>
-            <input type="password" class="validate" name="password" id="password" />
+          <div class=" center">
+            <button type="reset" id="reset" class="btn green waves-effect">Reset</button>
+            <button class="btn red waves-effect waves-light" type="submit">Login</button>
           </div>
-        </div>
-      </div>
-     
-    <a class="" href="{{ route('password.request') }}">
-            Forgot Your Password?
-        </a>
-        <br>
-        <br>
-        
-          <a class="alink" href="{{ route('register') }}">
-            Need an account? Register here.
-        </a>
-      <div class="card-action right-align">
-        <input type="reset" id="reset" class="btn green blue-text waves-effect" value="Reset">
-        <input type="submit" class="btn red waves-effect waves-light" value="Login">
-      </div>
-      
     </form>
+    <br>
+    <div class="card-action">
+      <a href="{{ route('password.request')}}">
+        Forgot Your Password?
+      </a>
+      <br>
+      <br>
+      <a href="{{ route('register')}}">
+        Need an account? Register here.
+      </a>
+    </div>
   </div>
 </div>
 {{--  <form class="box" method="POST" action="{{ route('login') }}">
-        {{ csrf_field() }}
-    <h1>Xpress Logistics Login</h1>
-    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-    <input type="email" name="email" id="email" placeholder="Email" value="{{ old('email') }}" required autofocus autocomplete="off">
-    @if ($errors->has('email'))
-    <span class="help-block">
-        <strong>{{ $errors->first('email') }}</strong>
-    </span>
-@endif
-<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+{{ csrf_field() }}
+<h1>Xpress Logistics Login</h1>
+<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+  <input type="email" name="email" id="email" placeholder="Email" value="{{ old('email') }}" required autofocus
+    autocomplete="off">
+  @if ($errors->has('email'))
+  <span class="help-block">
+    <strong>{{ $errors->first('email') }}</strong>
+  </span>
+  @endif
+  <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
     <input type="password" name="password" id="password" placeholder="Password" required autocomplete="off">
     @if ($errors->has('password'))
     <span class="help-block">
-        <strong>{{ $errors->first('password') }}</strong>
+      <strong>{{ $errors->first('password') }}</strong>
     </span>
-@endif
+    @endif
 
     <input type="submit" name="" value="Login">
     <a class="" href="{{ route('password.request') }}">
-            Forgot Your Password?
-        </a>
-        <br>
-        <br>
-        
-          <a class="alink" href="{{ route('register') }}">
-            If you do not have an account please register here.
-        </a>
-  </form>  --}}
-@endsection
+      Forgot Your Password?
+    </a>
+    <br>
+    <br>
+
+    <a href="{{ route('register') }}">
+      If you do not have an account please register here.
+    </a>
+    </form> --}}
+    @endsection
