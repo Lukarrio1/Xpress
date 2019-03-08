fc2f168d017e7dddf550a2fd46a2c5e4853c1bd2
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-03-07 20:01:23 -0500

    small fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,34 +1,33 @@
-@extends('layouts.app')
+@extends('layouts.forms')
 @section('content')
-
 <div class="valign-wrapper row login-box">
   <div class="col card hoverable s10 pull-s1 m6 pull-m3 l4 pull-l4">
     <form method="POST">
       <div class="card-content">
-        <h3 class="card-title  red-text">Xpress 
+        <h3 class="card-title red-text">Xpress 
           Logistics Register</h3>
           <div class="row">
-    <form class="col s12">
+    <div class="col s12">
       <div class="row">
         <div class="input-field col s6">
-          <input id="firstname" type="text" class="validate">
+          <input id="firstname" type="text" class="validate" name="firstname">
           <label for="firstname">First Name</label>
         </div>
         <div class="input-field col s6">
-          <input id="lastname" type="text" class="validate">
+          <input id="lastname" type="text" class="validate" name="lastname">
           <label for="last_name">Last Name</label>
         </div>
       </div>
             <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" class="validate" >
+          <input id="email" type="email" class="validate" name="email">
           <label for="email" data-error="wrong" data-success="right">Email</label>
         </div>
       </div>
 
       <div class="row">
           <div class="input-field col s12">
-            <input id="telephone" type="number" class="validate">
+            <input id="telephone" type="number" class="validate" name="phone">
           <label for="telephone">Telephone</label>
         </div>
       </div>
@@ -44,27 +43,27 @@
       </div>
       <div class="row">
                   <div class="input-field col s6">
-          <input placeholder="City" id="city" type="text" class="validate">
+          <input placeholder="City" id="city" type="text" class="validate" name="city">
           <label for="city">City</label>
                   </div>
                   {{--  <div class="input-field col s12 m6 ">  --}}
                         <label>Select Parish</label>
-                <select class="browser-default col s6">
+                <select class="browser-default col s6" name="parish">
                 <option value="" disabled selected>Choose your Parish</option>
-                <option value="1">Manchester</option>
-                <option value="2">Clarendon</option>
-                <option value="3">St.Catherine</option>
-                <option value="4">St.Andrew</option>
-                <option value="5">Kingston</option>
-                <option value="6">St.Thomas</option>
-                <option value="7">Portland</option>
-                <option value="8">St.Mary</option>
-                <option value="9">St.Ann</option>
-                <option value="10">Trelawny</option>
-                <option value="11">St.James</option>
-                <option value="12">Hanover</option>
-                <option value="13">Westmoreland</option>
-                <option value="14">St.Elizabeth</option>
+                <option value="Manchester">Manchester</option>
+                <option value="Clarendon">Clarendon</option>
+                <option value="St.Catherine">St.Catherine</option>
+                <option value="St.Andrew">St.Andrew</option>
+                <option value="Kingston">Kingston</option>
+                <option value="St.Thomas">St.Thomas</option>
+                <option value="Portland">Portland</option>
+                <option value="St.Mary">St.Mary</option>
+                <option value="St.Ann">St.Ann</option>
+                <option value="Trelawny">Trelawny</option>
+                <option value="St.James">St.James</option>
+                <option value="Hanover">Hanover</option>
+                <option value="Westmoreland">Westmoreland</option>
+                <option value="St.Elizabeth">St.Elizabeth</option>
                 </select>
 
              
@@ -72,15 +71,14 @@
               </div>
       <div class="row">
         <div class="input-field col s12">
-              <input placeholder="Country" id="country" type="text" class="validate">
+              <input placeholder="Country" id="country" type="text" class="validate" name="country">
           <label for="country">Country</label>
         </div>
       </div>
       <div class="row">
          <label>Preferred Pick Up Location</label> 
            <div class="input-field col s12">
-            
-    <select class="browser-default col 12">
+    <select class="browser-default col 12" name="p_location">
       <option value="" disabled selected>Select Pick Up Location</option>
       <option value="1">Home Deivery</option>
       <option value="2">Office Pick Up</option>
@@ -90,27 +88,24 @@
   </div>
         <p>
       <label>
-        <input for="termscheckbox" id="termscheckbox" type="checkbox" checked="checked" disabled="disabled" />
-        <span></span>
+        <input for="termscheckbox" id="termscheckbox" type="checkbox"/>
+        <span>aondnaidnawd</span>
       </label>
     </p>
-        </form>
       </div>
 
-          <div class="card-action center-align">
-           <input type="submit" class="btn red waves-effect waves-light white-text center" value="Submit">
+      <div class="card-action center-align">
+           <button type="submit" class="btn red waves-effect waves-light white-text center" >
+             Submit
+           </button>
       </div>
-      <a class="alink" href="{{ route('register') }}">
+      <a class="alink" href="{{ route('login') }}">
       Already have an account? Login here.
   </a>          
          </form>
         </div>
       </div>
-</div>
-        
-      </div>
-</div>
-</div>
+
 
 
 
