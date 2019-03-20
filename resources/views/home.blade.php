e73f5ec9d980404921b3666da7b0459448703e17
theboostkid <russelltimothy2@gmail.com>
2019-03-20 13:56:37 -0500

    Finished modal not coming up again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
             <div class="modal-content">
               <!-- Header -->
               <div class="modal-header">
-                <p class="heading lead">Modal Info</p>
+                <p class="heading lead">Welcome Tim</p>
               </div>
 
               <!-- Body -->
@@ -37,39 +37,52 @@
         <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item">
     <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
-      aria-selected="true">Home</a>
+      aria-selected="true">Overseas Address</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
-      aria-selected="false">Profile</a>
+      aria-selected="false">Pre Alert</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact"
-      aria-selected="false">Contact</a>
+      aria-selected="false">Notifications</a>
   </li>
+  
 </ul>
+<!-- Overseas address modal content -->
 <div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">Raw denim you
-    probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master
-    cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro
-    keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip
-    placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi
-    qui.</div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">Food truck fixie
-    locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit,
-    blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.
-    Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum
-    PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS
-    salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit,
-    sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
-    stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">Etsy mixtape
-    wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack
-    lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard
-    locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify
-    squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie
-    etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog
-    stumptown. Pitchfork sustainable tofu synth chambray yr.</div>
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+    <i class="fas fa-globe-americas fa-5x blue-text"></i><br>
+    <h2>Overseas Address</h2><br>
+    Name : Timothy Russell<br>
+    Address line 1: 2736 NW 29th Terrace,<br>
+    Address line 2: Bldg 13 Lauderdale Lakes,<br>
+    City / State : Oakland, Florida<br>
+    Zipcode : 33311    
+  </div>
+  <!-- Pre alert modal content -->
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+   <div class ="center">
+  <i class="far fa-bell fa-5x blue-text text-center"></i>   
+  </div> 
+    <h2>How To Create A Pre Alert</h2><br>
+      As soon as you receive your tracking number, create a pre alert.
+      This allows us to be able to to give you updates on your package as soon as it reaches our warehouse. 
+      Please note that all packages will be shipped once we have been provided with copies of the invoice. 
+      Creating a pre alert will reduce the risk of the package being returned due to the customer name being unidentified. 
+      Please ensure the purchase invoice is uploaded when creating the pre alert.
+  </div>
+  <!-- Overseas address modal content -->
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+    <i class="far fa-envelope fa-5x blue-text"></i><br>
+    <h2>Notifications</h2><br>
+    Track your package on your customer dashboard at every stage until it reaches you.
+     Also, you will be notified about your order status 
+     via email notifications and notifications on your dashboard.
+
+
+
+  </div>
 </div>
               </div>
 
