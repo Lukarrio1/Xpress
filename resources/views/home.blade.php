1b511250cb93353b4f1575ed1bb8139c26f84f04
theboostkid <russelltimothy2@gmail.com>
2019-03-22 16:53:25 -0500

    Buttons for password and picture

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -327,7 +327,7 @@
             <!--Panel-->
             <div class="card">
               <div class="card-header white-text primary-color">
-                Statistics
+                Recent Activity
               </div>
               <!--/.Card-->
               <div class="card-body pt-0 px-1">
@@ -359,292 +359,19 @@
 
           </div>
           <!--Grid column-->
-      </section>
+      </section> 
 
-
-
-
-      <!-- Section: Analytical panel -->
-
-      <!-- Section: data tables -->
-      <section class="pb-3">
-
-        <!-- Grid row -->
-        <div class="row">
-
-          <!-- Grid column -->
-          <div class="col-lg-6 col-md-12">
-  
-            <!-- Panel -->
-            <div class="card mb-4">
-
-              <div class="card-header white-text primary-color">
-                <h5 class="font-weight-500 my-1">Recent comments and replies</h5>
-              </div>
-
-              <div class="card-body">
-
-                <table class="table">
-                  <tbody>
-                    <tr>
-                      <td class="border-top-0">John Doe</td>
-                      <td class="border-top-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
-                      <td class="border-top-0 hour">
-                        <span class="grey-text float-right">
-                          <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                        </span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Merry Joe</td>
-                      <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
-                      <td class="hour">
-                        <span class="grey-text float-right">
-                          <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                        </span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Jessie Doe</td>
-                      <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</td>
-                      <td class="hour">
-                        <span class="grey-text float-right">
-                          <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                        </span>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-  
-                <button class="btn btn btn-flat grey lighten-3 btn-rounded waves-effect font-weight-bold dark-grey-text float-right btn-dash">View full report</button>
-  
-              </div>
-
-            </div>
-            <!-- Panel -->
-  
-            <!-- Panel -->
-            <div class="card mb-lg-0 mb-4">
-
-              <div class="card-header white-text primary-color">
-                <h5 class="font-weight-500 my-1">Issues</h5>
-              </div>
-
-              <div class="card-body">
-
-                <div class="table-responsive">
-                  <table class="table">
-                    <thead>
-                      <tr>
-                        <th class="font-weight-bold dark-grey-text">Status</th>
-                        <th class="font-weight-bold dark-grey-text">Title</th>
-                        <th class="font-weight-bold dark-grey-text">User</th>
-                        <th class="font-weight-bold dark-grey-text">Date</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td><span class="badge green">Open</span></td>
-                        <td>Lorem ipsum dolor</td>
-                        <td>John Doe</td>
-                        <td class="hour">
-                          <span class="grey-text">
-                            <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                          </span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><span class="badge green">Open</span></td>
-                        <td>Lorem ipsum dolor</td>
-                        <td>John Doe</td>
-                        <td class="hour">
-                          <span class="grey-text">
-                            <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                          </span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><span class="badge warning-color">In progress</span></td>
-                        <td>Lorem ipsum dolor</td>
-                        <td>John Doe</td>
-                        <td class="hour">
-                          <span class="grey-text">
-                            <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                          </span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><span class="badge red">Closed</span></td>
-                        <td>Lorem ipsum dolor</td>
-                        <td>John Doe</td>
-                        <td class="hour">
-                          <span class="grey-text">
-                            <i class="far fa-clock-o" aria-hidden="true"></i> 12 min
-                          </span>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-  
-              </div>
-
-            </div>
-            <!-- Panel -->
-  
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-lg-6 col-md-12">
-  
-            <!-- Panel -->
-            <div class="card mb-4">
-
-              <div class="card-header white-text primary-color">
-                <h5 class="font-weight-500 my-1">Users activity</h5>
-              </div>
-
-              <div class="card-body">
-
-                <div class="row my-2 pt-1">
-                  <div class="col-4">
-                    <small class="grey-text">Pages/Visits</small>
-                    <h4>139 419</h4>
-                  </div>
-                  <div class="col-4">
-                    <small class="grey-text">New visitors</small>
-                    <h4>51.94%</h4>
-                  </div>
-                  <div class="col-4">
-                    <small class="grey-text">Last week</small>
-                    <h4>51 932</h4>
-                  </div>
-                </div>
-                <div class="row mb-2">
-                  <div class="col-4">
-                    <small class="grey-text">Pages/Visits</small>
-                    <h4>139 419</h4>
-                  </div>
-                  <div class="col-4">
-                    <small class="grey-text">New visitors</small>
-                    <h4>51.94%</h4>
-                  </div>
-                  <div class="col-4">
-                    <small class="grey-text">Last week</small>
-                    <h4>51 932</h4>
-                  </div>
-                </div>
-                <div class="row mb-3">
-                  <div class="col-4">
-                    <small class="grey-text">Pages/Visits</small>
-                    <h4>139 419</h4>
-                  </div>
-                  <div class="col-4">
-                    <small class="grey-text">New visitors</small>
-                    <h4>51.94%</h4>
-                  </div>
-                  <div class="col-4">
-                    <small class="grey-text">Last week</small>
-                    <h4>51 932</h4>
-                  </div>
-                </div>
-  
-                <button class="btn btn btn-flat grey lighten-3 btn-rounded waves-effect font-weight-bold dark-grey-text float-right btn-dash">View full report</button>
-
-              </div>
-
-            </div>
-            <!-- Panel -->
-  
-            <!-- Panel -->
-            <div class="card text-center mb-4 pt-4 pb-3 red accent-2 white-text">
-              <i class="fas fa-bell fa-3x mb-3"></i>
-              <h4 class="h4-responsive">28 important messages</h4>
-            </div>
-            <!-- Panel -->
-  
-            <!-- Grid row -->
-            <div class="row">
-
-              <!-- Grid column -->
-              <div class="col-md-6 mb-md-0 mb-4">
-
-                <!-- Panel -->
-                <div class="card">
-                  
-                  <div class="card-header white-text grey darken-1">
-                    <h5 class="font-weight-500 my-1">Orders</h5>
-                  </div>
-
-                  <h6 class="ml-4 mb-0 pt-4 dark-grey-text font-weight-bold"><i class="fas fa-arrow-up blue-text mr-3" aria-hidden="true"></i> 2000 </h6>
-
-                  <div class="card-body">
-                    <div class="progress">
-                      <div class="progress-bar bg-primary" role="progressbar" style="width: 45%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <p class="font-small grey-text mt-3 mb-0">Better than last week (25%)</p>
-                  </div>
-
-                </div>
-                <!-- Panel -->
-
-              </div>
-              <!-- Grid column -->
-
-              <!-- Grid column -->
-              <div class="col-md-6 mb-0">
-
-                <!-- Panel -->
-                <div class="card">
-                  <div class="card-header white-text warning-color">
-                    <h5 class="font-weight-500 my-1">Monthly sales</h5>
-                  </div>
-
-                  <h6 class="ml-4 mb-0 pt-4 dark-grey-text font-weight-bold"><i class="fas fa-arrow-up blue-text mr-3" aria-hidden="true"></i> $ 2000</h6>
-
-                  <div class="card-body">
-                    <div class="progress">
-                      <div class="progress-bar bg-primary" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <p class="font-small grey-text mt-3 mb-0">Better than last week (25%)</p>
-                  </div>
-
-                </div>
-                <!-- Panel -->
-
-              </div>
-              <!-- Grid column -->
-
-            </div>
-            <!-- Grid row -->
-  
-          </div>
-          <!-- Grid column -->
-  
-        </div>
-        <!-- Grid row -->
-
-      </section>
-      <!-- Section: data tables -->
-
-    </div>
-
-          <!--Grid column-->
-
-        </div>
-        <!--Grid row-->
-
-      </section>
+    <section class = "pb-5">
       <!--Section: Classic admin cards-->
-      <Section class ="mb-3">
-        <!-- Third row -->
-
-            <!-- Third row -->
-            <div class="row mb-0">
+      
+              <div class="row mb-0 ">
 
               <!-- First column -->
               <div class="col-md-12">
+                <div class ="card bg-white">
+              
+                   
+               
 
                 <!-- Panel content -->
                 <div class="card-body pt-0">
@@ -726,60 +453,9 @@
 
                   </div>
                   <!-- Table -->
-
-                  <!-- Bottom Table UI -->
-                  <div class="d-flex justify-content-between">
-
-                    <!-- Name -->
-                    <select class="mdb-select colorful-select dropdown-primary mt-2">
-                      <option value="" disabled>Rows number</option>
-                      <option value="1" selected>5 rows</option>
-                      <option value="2">25 rows</option>
-                      <option value="3">50 rows</option>
-                      <option value="4">100 rows</option>
-                    </select>
-
-                    <!-- Pagination -->
-                    <nav class="my-4">
-                      <ul class="pagination pagination-circle pg-blue mb-0">
-
-                        <!-- First -->
-                        <li class="page-item disabled clearfix d-none d-md-block"><a class="page-link">First</a></li>
-
-                        <!-- Arrow left -->
-                        <li class="page-item disabled">
-                          <a class="page-link" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span class="sr-only">Previous</span>
-                          </a>
-                        </li>
-
-                        <!-- Numbers -->
-                        <li class="page-item active"><a class="page-link">1</a></li>
-                        <li class="page-item"><a class="page-link">2</a></li>
-                        <li class="page-item"><a class="page-link">3</a></li>
-                        <li class="page-item"><a class="page-link">4</a></li>
-                        <li class="page-item"><a class="page-link">5</a></li>
-
-                        <!-- Arrow right -->
-                        <li class="page-item">
-                          <a class="page-link" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span class="sr-only">Next</span>
-                          </a>
-                        </li>
-
-                        <!-- First -->
-                        <li class="page-item clearfix d-none d-md-block"><a class="page-link">Last</a></li>
-
-                      </ul>
-                    </nav>
-                    <!-- Pagination -->
-
-                  </div>
-                  <!-- Bottom Table UI -->
-
-      </Section>
+                </div>
+                </div>
+      </section>
 
     </div>
 @endsection
