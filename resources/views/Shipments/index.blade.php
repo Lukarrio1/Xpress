636912694261d10c239c36c91c034af0739c2582
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-03-25 22:42:55 -0500

    shipment page created

--- resources/views/Shipments/index.blade.php
+++ resources/views/Shipments/index.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-    this is the shipment page... add your stuff here....
+    this is the shipment page... add your stuff here.... right here fham right here
 @endsection
