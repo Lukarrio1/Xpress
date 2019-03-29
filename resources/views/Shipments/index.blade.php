219fe6290a227fd7c48f9e7ec2308fcd30d333ff
theboostkid <russelltimothy2@gmail.com>
2019-03-29 17:25:10 -0500

    Shipments page

--- resources/views/Shipments/index.blade.php
+++ resources/views/Shipments/index.blade.php
@@ -1,6 +1,76 @@
 @extends('layouts.app')
 @section('content')
-    this is the shipment page... add your stuff here.... right here fham right here
+<!--Table-->
+<table class="table table-striped w-100">
+
+  <!--Table head-->
+  <thead>
+    <tr>
+      <th>Tracking No.</th>
+      <th>Reference No.</th>
+      <th>Recipient</th>
+      <th>Description</th>
+      <th>Delivery Date</th> 
+      <th>Shipping Charge</th>
+      <th>Status</th>
+      <th>Created At</th>
+      <th>Updated At</th>
+    </tr>
+  </thead>
+  <!--Table head-->
+
+  <!--Table body-->
+  <tbody>
+    <tr class="table-info">
+      <th scope="row">1</th>
+      
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      
+      
+    </tr>
+    <tr class="table-info">
+      <th scope="row">3</th>
+      
+      
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      
+      
+    </tr>
+    <tr class="table-info">
+      <th scope="row">5</th>
+      
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      
+      
+    </tr>
+    <tr class="table-info">
+      <th scope="row">7</th>
+      
+      
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      
+      
+    </tr>
+    <tr class="table-info">
+      <th scope="row">9</th>
+      
+      
+    </tr>
+  </tbody>
+  <!--Table body-->
+
+
+</table>
+<!--Table-->
 
     
 @endsection
