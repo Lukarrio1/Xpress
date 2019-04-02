129e96ed17e905a63f6d6ebd21d5abd5ddf93e68
theboostkid <russelltimothy2@gmail.com>
2019-04-02 16:37:52 -0500

    Admin Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -93,6 +93,31 @@
         </div>
         <!-- Central Modal Large Info -->
 
+          <!-- Modal -->
+          <div class="modal fade" id="taskmodal" tabindex="-1" role="dialog" aria-labelledby="taskmodal"
+            aria-hidden="true">
+
+            <!-- Add .modal-dialog-centered to .modal-dialog to vertically center the modal -->
+        <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Add New Task</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+        
+
           <!--Section: Classic admin cards-->
       <section>
 
