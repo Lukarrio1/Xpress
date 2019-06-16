fd018bfa82daeef9a1d5cd94772ecc37541e157f
theboostkid <russelltimothy2@gmail.com>
2019-06-15 19:18:56 -0500

    Stuff

--- resources/views/admin/Messages.blade.php
+++ resources/views/admin/Messages.blade.php
@@ -1,234 +1,297 @@
 @extends('layouts.admin')
 @section('content')
-<div class="container-fluid">
 
-      <!-- Section: Chat -->
-      <section>
+<style>
+.inbox {
+  background: url("https://picsum.photos/3000/3000");
+  background-size: cover;
+  background-position: 50% 50%;
+  min-height: 100vh;
+  display: flex;
+}
+.inbox aside {
+  display: flex;
+  padding-right: 20px;
+  min-height: 100%;
+  transition: all ease-in-out 0.2s;
+}
+.inbox aside ul {
+  padding-left: 0;
+}
+.inbox aside ul li {
+  list-style-type: none;
+  cursor: pointer;
+  padding: 10px 25px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #000;
+  padding-left: 25px;
+  flex-grow: 1;
+  flex-shrink: 1;
+  margin: 10px 0;
+}
+.inbox aside ul li:hover {
+  color: #fff;
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 0 10px 10px 0;
+}
+.inbox aside ul li i {
+  color: #000;
+  margin-right: 10px;
+  font-size: 20px;
+}
+.inbox aside ul li i:hover {
+  color: #d7d5d8;
+}
+.inbox aside ul li:first-child {
+  background: rgba(255, 255, 255, 0.3);
+  border-radius: 0 10px 10px 0;
+}
+.inbox .messages {
+  flex-grow: 2;
+  overflow: hidden;
+  position: relative;
+  margin: 0 10px;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.inbox .messages .message {
+  width: 100%;
+  top: 0;
+  right: 50%;
+  -webkit-transform: translateY(50%);
+          transform: translateY(50%);
+  background: rgba(255, 255, 255, 0.5);
+  margin-bottom: 1px;
+  border: 1px solid #bfbebe;
+  cursor: pointer;
+  vertical-align: middle;
+  padding: 15px;
+  display: flex;
+}
+.inbox .messages .message li {
+  list-style: none;
+}
+.inbox .messages .emotions {
+  flex-grow: 1;
+  flex-direction: flex-start;
+}
+.inbox .messages .sender {
+  flex-grow: 1;
+  justify-content: flex-start;
+}
+.inbox .messages .title {
+  flex-grow: 2;
+  flex-direction: flex-end;
+}
+.inbox .messages .date {
+  flex-grow: 1;
+  flex-direction: flex-end;
+}
+.inbox .messages .date span {
+  font-weight: bold;
+}
 
-        <!-- Grid row -->
-        <div class="row">
 
-          <!-- Grid column -->
-          <div class="col-lg-4">
+</style>
 
-            <!-- Name -->
-            <div class="md-form mb-4">
-              <i class="fas fa-search prefix"></i>
-              <input type="text" id="searchConv" class="form-control">
-              <label for="searchConv" class="">Search Message</label>
-            </div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Inbox</title>
+</head>
+<body>
+  <main class='inbox'>
+    <aside>
+      <ul>
+        <li>
+          <i class="fa fa-inbox" aria-hidden="true"></i> Inbox <span>1024</span>
+        </li>
+        <li>
+          <i class="fa  fa-star" aria-hidden="true"></i> Starred 
+        </li>
+        <li>
+          <i class="fa fa-clock-o"></i> Snoozed 
+        </li>
+        <li>
+          <i class="fa fa-inbox" aria-hidden="true"></i> Important 
+        </li>
+        <li>
+          <i class="fa fa-inbox" aria-hidden="true"></i> Sent 
+        </li>
+        
+      </ul>
+    </aside>
+    <section class="messages">
+      
 
-            <!-- Messages -->
-            <div class="list-group">
+      
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+        </span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
 
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media active">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (32).jpg">
+      
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.                 Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.                 Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.                 Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.                 Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.                 Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
+      <ul class="message">
+        <li class="emotions">
+          <span>
+           <input type="checkbox">
+          </span>
+          <span><i class="fa fa-star-o" aria-hidden="true"></i>
+</span>
+        </li>
+        <li class='sender'>
+          <b>Usplash Team</b>
+        </li>
+        <li class="title">
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.                 Qui, corporis vitae inventore odit 
+          </b>
+        </li>
+        <li class="date">
+          <span>
+            4 Dec
+          </span>
+        </li>
+      </ul>
 
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Johny Dannish</strong></hp>
-                  <small>13 July</small>
-                </div>
 
-                <!-- Message -->
-                <p class="text-truncate"><strong>You: </strong> Donec id elit non mi porta gravida at eget metus.
-                  Maecenas
-                  sed diam eget risus varius blandit.</p>
-              </a>
 
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (21).jpg">
+      
+      
+    </section>
+  </main>
+</body>
 
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Mario Sonetti</strong></hp>
-                  <small>14 July</small>
-                </div>
-
-                <!-- Message -->
-                <p class="text-truncate"><span class="badge red">MDB Team</span> <strong>Mario: </strong> Donec id
-                  elit non
-                  mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-              </a>
-
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (20).jpg">
-
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Catherine Blake</strong></hp>
-                  <small>15 July</small>
-                </div>
-
-                <!-- Message -->
-                <p class="text-truncate"><strong>Catherine: </strong> Donec id elit non mi porta gravida at eget metus.
-                  Maecenas
-                  sed diam eget risus varius blandit.</p>
-              </a>
-
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (26).jpg">
-
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Anna Deynah</strong></hp>
-                  <small>16 July</small>
-                </div>
-
-                <!-- Message -->
-                <p class="text-truncate"><strong>You: </strong> Donec id elit non mi porta gravida at eget metus.
-                  Maecenas
-                  sed diam eget risus varius blandit.</p>
-              </a>
-
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (29).jpg">
-
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Marie Joe</strong></hp>
-                  <small>16 July</small>
-                </div>
-
-                <!-- Message -->
-                <p class="text-truncate"><strong>Marie: </strong> Donec id elit non mi porta gravida at eget metus.
-                  Maecenas
-                  sed diam eget risus varius blandit.</p>
-              </a>
-
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (22).jpg">
-
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Amanda Hayle</strong></hp>
-                  <small>16 July</small>
-                </div>
-
-                <!-- Message -->
-                <p class="text-truncate"><strong>Amanda: </strong> Donec id elit non mi porta gravida at eget metus.
-                  Maecenas
-                  sed diam eget risus varius blandit.</p>
-              </a>
-
-              <!-- Single message -->
-              <a href="#" class="list-group-item list-group-item-action media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (3).jpg">
-
-                <!-- Author -->
-                <div class="d-flex justify-content-between mb-1 ">
-                  <hp class="mb-1"><strong>Freddie Wolf</strong></hp>
-                  <small>16 July</small>
-                </div>
-
-                <!-- Message -->
-                <p class="text-truncate"><strong>You: </strong> Donec id elit non mi porta gravida at eget metus.
-                  Maecenas
-                  sed diam eget risus varius blandit.</p>
-              </a>
-
-            </div>
-            <!-- Messages -->
-
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-lg-8 mt-lg-0 my-5">
-
-            <!-- Conversation -->
-            <div class="border border-dark p-4">
-
-              <!-- My Message -->
-              <div class="text-center"><small>16 July, 23:54</small></div>
-              <div class="d-flex justify-content-end">
-                <p class="primary-color rounded p-3 text-white w-75 ">Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Molestiae modi exercitationem
-                  dignissimos repellat, voluptas iure quod aliquid voluptatem perspiciatis quidem
-                  sit eos, cum fugit voluptatibus quos laboriosam sed tenetur voluptate!</p>
-              </div>
-
-              <!-- Your Message -->
-              <div class="text-center"><small>16 July, 23:55</small></div>
-              <div class="d-flex justify-content-start media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (32).jpg">
-
-                <p class="grey lighten-3 rounded p-3 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Molestiae modi exercitationem
-                  dignissimos repellat, voluptas iure quod aliquid voluptatem perspiciatis quidem
-                  sit eos, cum fugit voluptatibus quos laboriosam sed tenetur voluptate!</p>
-              </div>
-
-              <!-- Your Message -->
-              <div class="text-center"><small>16 July, 23:56</small></div>
-              <div class="d-flex justify-content-start media">
-                <!-- Avatar -->
-                <img class="mr-3 avatar-sm float-left" src="https://mdbootstrap.com/img/Photos/Avatars/img (32).jpg">
-
-                <p class="grey lighten-3 rounded p-3 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Molestiae modi exercitationem
-                  dignissimos repellat, voluptas iure quod aliquid voluptatem perspiciatis quidem
-                  sit eos, cum fugit voluptatibus quos laboriosam sed tenetur voluptate!</p>
-              </div>
-
-              <!-- My Message -->
-              <div class="text-center"><small>16 July, 23:54</small></div>
-              <div class="d-flex justify-content-end">
-                <p class="primary-color rounded p-3 text-white w-75 ">Lorem ipsum dolor sit amet, consectetur
-                  adipisicing elit. Molestiae modi exercitationem
-                  dignissimos repellat, voluptas iure quod aliquid voluptatem perspiciatis quidem
-                  sit eos, cum fugit voluptatibus quos laboriosam sed tenetur voluptate!</p>
-              </div>
-
-              <!-- New message -->
-              <div class="row">
-
-                <div class="col-md-12">
-
-                  <div class="d-flex flex-row">
-
-                    <div class="md-form chat-message-type">
-                      <textarea type="text" id="form7" class="md-textarea form-control" rows="3"></textarea>
-                      <label for="form7">Type your message</label>
-                    </div>
-
-                    <div class="mt-5">
-                      <a class="btn btn-primary btn-lg">Send</a>
-                    </div>
-
-                  </div>
-
-                </div>
-
-              </div>
-              <!-- New message -->
-
-            </div>
-            <!-- Conversation -->
-
-          </div>
-          <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-      </section>
-      <!-- Section: Chat -->
-
-    </div>
     
 @endsection
--- resources/views/admin/updateshipment/Updateshipments.blade.php
+++ resources/views/admin/updateshipment/Updateshipments.blade.php
@@ -1,6 +1,161 @@
 @extends('layouts.admin')
 @section('content')
-<style>
+ <style>
+}
+.input-group.md-form.form-sm.form-2 input.red-border  {
+    border: 1px solid red;
+}
+
+</style>
+   <div class="container-fluid">
+
+      
+
+<section> 
+           
+  <!--Section heading-->
+      <div class="mt-lg-5 mb-5 mt--3">
+        <h4 class=" mt--3text-left font-weight-bold dark-grey-text">Update Shipments</h4>
+        <p class="grey-text mt-3">Add all client new shipments info below. </p>
+        <hr>
+      </div>
+              <div class="input-group md-form form-sm form-2 pl-0">
+  <input class="form-control my-0 py-1 danger-border" type="text" placeholder="Search" aria-label="Search">
+  <div class="input-group-append">
+    <span class="input-group-text red lighten-2" id="basic-text1"><i class="fas fa-search text-grey"
+        aria-hidden="true"></i></span>
+  </div>
+<!-- Top Table UI -->
+
+        <div class="card card-cascade narrower z-depth-1">
+
+          <!-- Card image -->
+          <div class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
+
+            <div>
+              {{--  <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-th-large mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-columns mt-0"></i></button>
+              --}}
+              </div>
+
+            <a href="" class="white-text mx-3">Table name</a>
+
+            <div>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-pencil-alt mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-eraser mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-info-circle mt-0"></i></button>
+            </div>
+
+          </div>
+          <!-- /Card image -->
+
+          <div class="px-4">
+
+            <div class="table-responsive">
+              <!-- Table -->
+              <table class="table table-hover mb-0">
+
+                <!-- Table head -->
+                <thead>
+                  <tr>
+                    <th>
+                      <input class="form-check-input" type="checkbox" id="checkbox">
+                      <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
+                    </th>
+                    <th class="th-lg"><a>First Name <i class="fas fa-sort ml-1"></i></a></th>
+                    <th class="th-lg"><a href="">Last Name<i class="fas fa-sort ml-1"></i></a></th>
+                    <th class="th-lg"><a href="">Username<i class="fas fa-sort ml-1"></i></a></th>
+                    <th class="th-lg"><a href="">Email<i class="fas fa-sort ml-1"></i></a></th>
+                    <th class="th-lg"><a href="">Country<i class="fas fa-sort ml-1"></i></a></th>
+                    <th class="th-lg"><a href="">City<i class="fas fa-sort ml-1"></i></a></th>
+                  </tr>
+                </thead>
+                <!-- Table head -->
+
+                <!-- Table body -->
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      <input class="form-check-input" type="checkbox" id="checkbox1">
+                      <label class="form-check-label" for="checkbox1" class="label-table"></label>
+                    </th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>markotto@gmail.com</td>
+                    <td>USA</td>
+                    <td>San Francisco</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input class="form-check-input" type="checkbox" id="checkbox2">
+                      <label class="form-check-label" for="checkbox2" class="label-table"></label>
+                    </th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>jacobt@gmail.com</td>
+                    <td>France</td>
+                    <td>Paris</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input class="form-check-input" type="checkbox" id="checkbox3">
+                      <label class="form-check-label" for="checkbox3" class="label-table"></label>
+                    </th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td><button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-th-large mt-0"></i></button>
+              <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2"><i class="fas fa-columns mt-0"></i></button>
+             </td>
+                    <td>Germany</td>
+                    <td>Berlin</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input class="form-check-input" type="checkbox" id="checkbox4">
+                      <label class="form-check-label" for="checkbox4" class="label-table"></label>
+                    </th>
+                    <td>Paul</td>
+                    <td>Topolski</td>
+                    <td>@P_Topolski</td>
+                    <td>ptopolski@gmail.com</td>
+                    <td>Poland</td>
+                    <td>Warsaw</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <input class="form-check-input" type="checkbox" id="checkbox5">
+                      <label class="form-check-label" for="checkbox5" class="label-table"></label>
+                    </th>
+                    <td>Anna</td>
+                    <td>Doe</td>
+                    <td>@andy</td>
+                    <td>annadoe@gmail.com</td>
+                    <td>Spain</td>
+                    <td>Madrid</td>
+                  </tr>
+                </tbody>
+                <!-- Table body -->
+              </table>
+              <!-- Table -->
+            </div>
+
+            <hr class="my-0">
+
+            
+
+          </div>
+        </div>
+
+      </section>
+
+
+
+
+
+{{--  {{--  <style>
 }
 .input-group.md-form.form-sm.form-2 input.red-border  {
     border: 1px solid red;
@@ -58,9 +213,16 @@
                           <thead>
                             <tr>
                               <th>XL#:</th>
-                              <th>Name</th>
-                              <th>Telephone</th>
+                              <th>Recipient</th>
                               <th>Email</th>
+                              <th>Tracking No.</th>
+                              <th>Reference No.</th>
+                              <th>Description</th>
+                              <th>Delivery Date</th>
+                              <th>Arrival Date</th>
+                              <th>Shipping Charge</th>
+                              <th>Status</th>
+                              <th>Created At</th>
                               <th>Actions</th>
                             </tr>
                           </thead>
@@ -178,7 +340,7 @@
   
 
 
+  --}}
 
-
-  <!-- Main layout -->
+  <!-- Main layout -->  --}}
 @endsection
