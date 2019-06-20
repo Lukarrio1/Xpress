b1aa7796064016cdbc78cf2ada2cf072b4c597eb
lukarrio1 <tomennis1997@gmail.com>
2019-06-20 14:55:15 -0500

    user page updated

--- resources/views/admin/Allusers.blade.php
+++ resources/views/admin/Allusers.blade.php
@@ -1,62 +1,60 @@
-@extends('layouts.admin')
-@section('content')
-    <div class="container-fluid">
-
-      <!-- Section: Customers -->
-      <section class="section team-section">
-
-        <!-- First row -->
+@extends('layouts.admin') @section('content')
+<div class="container-fluid">
+  <!-- Section: Customers -->
+  <section class="section team-section">
+    <!-- First row -->
+    <div class="row">
+      <!-- First column -->
+      <div class="col-md-8">
+        <div class="row mb-1">
+          <div class="col-md-9">
+            <h4 class="h4-responsive mt-1">Your Clients</h4>
+          </div>
+          <div class="col-md-3">
+            <div class="md-form">
+              <input
+                placeholder="Search..."
+                type="text"
+                class="form-control"
+                id="usersearch"
+              />
+              {{-- <button class="fas-fa-search"></button> --}}
+            </div>
+          </div>
+        </div>
         <div class="row">
-
-          <!-- First column -->
-          <div class="col-md-8">
-
-            <div class="row mb-1">
-              <div class="col-md-9">
-                <h4 class="h4-responsive mt-1">Your Clients</h4>
+          <div class="col-md-12 mb-1">
+            <!-- Tabs -->
+            <div class="card card-cascade narrower z-depth-1 mt-5">
+              <div
+                class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center"
+              >
+                <div></div>
+                <a class="white-text align-content-center mx-3 h4"
+                  >All Users
+                </a>
+                <div></div>
               </div>
-              <div class="col-md-3">
-                <div class="md-form">
-                <input placeholder="Search..." type="text" class="form-control" id="usersearch">
-                {{-- <button class="fas-fa-search"></button> --}}
-                </div>
+              <div class="text-center border border-light p-5">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>XL#:</th>
+                      <th>Name</th>
+                      <th>telephone</th>
+                      <th>Email</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody id="alluserbody"></tbody>
+                </table>
               </div>
             </div>
-            <div class="row">
-              <div class="col-md-12 mb-1">
-                <!-- Tabs -->
-                <div class="card card-cascade narrower z-depth-1 mt-5">
-    <div
-        class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
-        <div>
+          </div>
         </div>
-        <a class="white-text align-content-center mx-3 h4">News Form
-        </a>
-        <div>
-        </div>
+      </div>
+      <div class="col-md-4 mb-md-0 mb-5" id="usercardbody">
     </div>
-    <div class="text-center border border-light p-5">
-        <table class="table">
-                          <thead>
-                            <tr>
-                              <th>XL#:</th>
-                              <th>Name</th>
-                              <th>telephone</th>
-                              <th>Email</th>
-                              <th>Actions</th>
-                            </tr>
-                          </thead>
-                          <tbody id="alluserbody">
-                          
-                          </tbody>
-                        </table>
-
-
-        
-    </div>
+  </section>
 </div>
-                
-                  </div>
-                  <!-- /.Tabs -->
-                
-@endsection
+       @endsection
