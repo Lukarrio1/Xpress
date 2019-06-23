938bf27fe36f4ed809d7051d51ef46a1bd322e67
lukarrio1 <tomennis1997@gmail.com>
2019-06-23 16:03:09 -0500

    test -2

--- public/js/admin.js
+++ public/js/admin.js
@@ -921,6 +921,7 @@ Allshipments = () => {
   $.get("/admin/shipments/all", data => {
     let shipment = jQuery.parseJSON(data);
     console.log(shipment);
+    console.log("its here");
     let shipp = shipment.filter(n => n.collected == 0);
     $("#shipp").html(`${shipp.length}`);
     $("#shipa").html(`${shipment.length}`);
