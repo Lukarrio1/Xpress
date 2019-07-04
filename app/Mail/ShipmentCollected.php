5456a023c78063b958436672f2e74b599d20215a
lukarrio1 <tomennis1997@gmail.com>
2019-07-04 00:03:54 -0500

    email integration

---
+++ app/Mail/ShipmentCollected.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ShipmentCollected extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $reference;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($reference)
+    {
+        $this->reference =$reference;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('components.ShipmentCollected');
+    }
+}
