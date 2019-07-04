a91835dfbffa63632def2ad8fa37d67c0ce72918
lukarrio1 <tomennis1997@gmail.com>
2019-07-03 20:15:06 -0500

    footer removed

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -38,7 +38,6 @@
     <main>
       @yield('content')
     </main>
-    @include('inc.adminfooter')
     <script
       src="http://code.jquery.com/jquery-3.3.1.min.js"
       integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
