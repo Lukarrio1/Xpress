5aa0660146b09ba16a5f8eeb9409f1a1918e6ccc
lukarrio1 <tomennis1997@gmail.com>
2019-07-05 00:29:02 -0500

    small fix

--- public/js/admin.js
+++ public/js/admin.js
@@ -188,7 +188,6 @@ Allusers = () => {
 
 AdminVerifiedUser = users => {
   let verified = users.filter(n => n.verified !== '').length;
-  console.log(verified);
   $('#verifieduserscount').html(`${verified}`);
   $('#totalusercount').html(`${users.length}`);
 };
